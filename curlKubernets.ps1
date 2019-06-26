01558dc9c574a08c7e648fb79462db86b1d35b94
alexmickelson <alexmickelson96@gmail.com>
2019-06-26 16:44:30 -0600

    cleaned up powershell tools

--- curlKubernets.ps1
+++
@@ -1,8 +0,0 @@
-
-# param (
-#     [Parameter(mandatory=$true)][Alias("curlTarget")]
-#     [string]$target
-# )
-
-kubectl run -it --rm curl --restart=Never --image=nbrown/curl -- $args[0]
-Write-Output ""
--- powershellSetup.ps1
+++ powershellSetup.ps1
@@ -6,3 +6,8 @@ function global:commit {
     git commit -m $args[0]
     git push
 }
+
+function global:kubecurl {
+    kubectl run -it --rm curl --restart=Never --image=nbrown/curl -- $args[0]
+    Write-Output ""
+}
