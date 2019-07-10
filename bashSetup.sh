25b0fd5c66bbaeb0659f18e9f80845fa593f87be
alex mickelson <alexmickelson96@gmail.com>
2019-07-10 09:49:32 -0600

    worked on bash setup

--- bashSetup.sh
+++ bashSetup.sh
@@ -1,4 +1,18 @@
+#!/bin/bash
+
+#make sure this file runs every time you start bash (on startup)
+grep -qxF '~/tools/bashSetup.sh' ~/.bashrc || echo '~/tools/bashSetup.sh' >> ~/.bashrc
 
 cp config/.vimrc ~/
+#remember git username and password for 1 hr (WARNING: stored in plaintext for that hour)
 git config --global credential.helper 'cache --timeout=3600'
+
+#adjust path to include snaps (only if /snap/bin/ exists)
+[ -d "/snap/bin/" ] && export PATH=$PATH:/snap/bin/
+
+#install lazydocker if not installed
+command -v curl > /dev/null || echo "Install curl to install lazydocker"
+command -v lazydocker > /dev/null || ~/tools/installLazyDocker.sh
+
+
 
