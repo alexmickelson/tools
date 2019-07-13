e29ee5f6a670ea7426a88120a6865e139fd146bc
alexmickelson <alexmickelson96@gmail.com>
2019-07-13 12:26:51 -0600

    added youtube-dl function

--- powershellSetup.ps1
+++ powershellSetup.ps1
@@ -11,3 +11,8 @@ function global:kubecurl {
     kubectl run -it --rm curl --restart=Never --image=nbrown/curl -- $args[0]
     Write-Output ""
 }
+
+function global:ydl {
+    youtube-dl --extract-audio --audio-format mp3 $args[0]
+    Write-Output ""
+}
