f6d0e1ffd52797e6d90fee7ba47ed6cfcaebcb9e
alexmickelson <alexmickelson96@gmail.com>
2019-07-13 12:36:26 -0600

    added git prompt ubuntu

--- bashSetup.sh
+++ bashSetup.sh
@@ -10,6 +10,14 @@ git config --global credential.helper 'cache --timeout=3600'
 #adjust path to include snaps (only if /snap/bin/ exists)
 [ -d "/snap/bin/" ] && export PATH=$PATH:/snap/bin/
 
+#make sure git-prompt is installed 
+[ -d "$HOME/.bash-git-prompt" ] || git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
+if [ -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
+    GIT_PROMPT_ONLY_IN_REPO=1
+    source $HOME/.bash-git-prompt/gitprompt.sh
+    GIT_PROMPT_THEME=Single_line_Ubuntu
+fi
+
 #install lazydocker if not installed
 command -v curl > /dev/null || echo "Install curl to install lazydocker"
 command -v lazydocker > /dev/null || ~/tools/installLazyDocker.sh
