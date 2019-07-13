73826f21450a5d22a9f8e0b8e0dc4b4e76a4cb94
alexmickelson <alexmickelson96@gmail.com>
2019-07-13 12:57:50 -0600

    added git prompt ubuntu

--- bashSetup.sh
+++ bashSetup.sh
@@ -12,13 +12,13 @@ git config --global credential.helper 'cache --timeout=3600'
 
 #make sure git-prompt is installed 
 [ -d "$HOME/.bash-git-prompt" ] || git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
-#grep -qxF '$HOME/tools/bashSetup.sh' $HOME/.bashrc
-echo '
+grep -qxF 'if [ -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then' $HOME/. || echo '
 if [ -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
     GIT_PROMPT_ONLY_IN_REPO=1
     source $HOME/.bash-git-prompt/gitprompt.sh
     GIT_PROMPT_THEME=Single_line_Ubuntu
-fi'
+fi
+' >> $HOME/.bashrc
 
 #install lazydocker if not installed
 command -v curl > /dev/null || echo "Install curl to install lazydocker"
