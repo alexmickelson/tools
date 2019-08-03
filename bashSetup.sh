6e66c1fe85f9b84fb455c838354adc6ae39a50ac
Alex Mickelson <alexmickelson96@gmail.com>
2019-08-03 16:09:37 -0600

    "added commit script"

--- bashSetup.sh
+++ bashSetup.sh
@@ -3,12 +3,15 @@
 #make sure this file runs every time you start bash (on startup)
 grep -qxF '$HOME/tools/bashSetup.sh' $HOME/.bashrc || echo '$HOME/tools/bashSetup.sh' >> $HOME/.bashrc
 
+#set up aliases
+grep -qxF 'alias commit="$HOME/tools/scripts/commit.sh"' $HOME/.bashrc || echo 'alias commit="$HOME/tools/scripts/commit.sh"' >> $HOME/.bashrc
+
 #cp ~/tools/config/.vimrc ~/
 #remember git username and password for 1 hr (WARNING: stored in plaintext for that hour)
 git config --global credential.helper 'cache --timeout=3600'
 
 #adjust path to include snaps (only if /snap/bin/ exists)
-[ -d "/snap/bin/" ] && export PATH=$PATH:/snap/bin/
+[ -d "/snap/bin/" ] && echo "$PATH"|grep -q "/snap/bin" && export PATH=$PATH:/snap/bin/
 
 #make sure git-prompt is installed 
 [ -d "$HOME/.bash-git-prompt" ] || git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
@@ -18,7 +21,7 @@ if [ -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
     source $HOME/.bash-git-prompt/gitprompt.sh
     GIT_PROMPT_THEME=Single_line_Ubuntu
 fi
-' >> $HOME/.bashrc
+' >> $HOME/.bashrcS
 
 #install lazydocker if not installed
 command -v curl > /dev/null || echo "Install curl to install lazydocker"
