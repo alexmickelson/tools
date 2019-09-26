c9a48509fbe0d93547be124c2f047dac9e3676c8
Alex Mickelson <alexmickelson96@gmail.com>
2019-09-26 09:55:48 -0600

    changed ssh setup

--- ssh-setup.sh
+++ ssh-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh-keyget -t rsa -N "" -f ~/.ssh/id_rsa
+#ssh-keyget -t rsa -N "" -f ~/.ssh/id_rsa
 
 for sshdst in "$@"; do
     ssh $sshdst "touch ~/.ssh/authorized_keys; $(cat ~/.ssh/id_rsa.pub) >> ~/.ssh/authorized_keys" 
